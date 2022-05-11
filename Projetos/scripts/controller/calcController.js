ff184f595e7f9f248d9eb62e4fdebd831f49b8c6
EduardoKayke <hakuzosu.developer@gmail.com>
2022-05-11 15:25:31 -0300

    Class controller

---
+++ Projetos/scripts/controller/calcController.js
@@ -0,0 +1,29 @@
+class CalcController {
+
+    constructor(){        
+        this._displayCalc = '0';
+        this._currentDate;
+        this.initialize();
+    };
+
+    initialize(){
+
+    };
+
+    get displayCalc(){
+        return this._displayCalc;
+    };
+
+    set displayCalc(value){
+        this._displayCalc = value;
+    };
+
+    get dataAtual(){
+        return this._currentDate;
+    };
+
+    set dataAtual(value){
+        this._currentDate = value;
+    };
+
+};
